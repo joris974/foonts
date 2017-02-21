fdb21e4967063bf5e5f16edcdfa9309f714fd357
Joris Buchou <joris.buchou@gmail.com>
2017-02-20 17:39:27 -0800

    Change directory structure

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-import {Router, Route, hashHistory} from 'react-router';
-
-import ReactDOM from 'react-dom'
-import React from 'react'
-import App from './app.js'
-
-const mountNode = document.getElementById("main")
-
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/:fonts" component={App}/>
-  </Router>,
-  mountNode
-)
+import Router from './routers/router.js'
---
+++ js/routers/router.js
@@ -0,0 +1,15 @@
+import {Router, Route, hashHistory} from 'react-router';
+
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './../components/app.js'
+
+const mountNode = document.getElementById("main")
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/:fonts" component={App}/>
+  </Router>,
+  mountNode
+)
