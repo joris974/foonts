96ff780b16c4965a1d99bf464d8248ad32cd8d21
Joris Buchou <joris.buchou@gmail.com>
2017-02-25 11:52:45 -0800

    Add Search and checkbox filter for fonts

--- js/components/fonts-page.js
+++ js/components/fonts-page.js
@@ -30,10 +30,38 @@ const FontItem = (props) => {
   )
 }
 
+const filterFontList = function(fontList, searchInput, fontCategories) {
+  return _
+    .chain(fontList)
+    .filter(font => {
+      if(_.isEmpty(searchInput)) {
+        return true
+      } else {
+        return !_.isEmpty(font.family.trim().toLowerCase().match(searchInput.trim().toLowerCase()))
+      }
+
+    })
+    .filter(font => {
+      return _.includes(fontCategories, font.category)
+    })
+    .value()
+}
+
+const isChecked = function(fontCategories, category) {
+  return _.includes(fontCategories, category)
+}
+
+const allCategories = () => ["display", "serif", "sans-serif", "monospace", "handwriting"]
+
 class FontsPage extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { numMaxVisible: 12 }
+    super(props)
+    this.state =
+      { numMaxVisible: 12
+      , searchInput: ""
+      , filteredFontList: props.fontList
+      , fontCategories: allCategories()
+      }
   }
 
   onClickSeeMore() {
@@ -42,12 +70,39 @@ class FontsPage extends React.Component {
     )
   }
 
+  handleChangeSearch(e) {
+    const {fontCategories} = this.state
+    const searchInputVal = e.target.value
+    const searchInput = _.isEmpty(searchInputVal) ? "" : searchInputVal
+    const filteredFontList = filterFontList(this.props.fontList, searchInput, fontCategories)
+    this.setState(
+      { searchInput
+      , filteredFontList
+      }
+    )
+  }
+
+  handleChangeCheckbox(category){
+    const {fontCategories, searchInput} = this.state
+
+    const newFontCategories =
+      _.includes(fontCategories, category) ?
+        _.filter(fontCategories, c => c !== category) :
+        fontCategories.concat([category])
+
+    const filteredFontList = filterFontList(this.props.fontList, searchInput, newFontCategories)
+    this.setState(
+      { fontCategories: newFontCategories
+      , filteredFontList
+      }
+    )
+  }
+
   render() {
-    const {fontList} = this.props
-    const {numMaxVisible} = this.state
+    const {numMaxVisible, searchInput, filteredFontList, fontCategories} = this.state
 
     const fontsNode = _
-      .chain(fontList)
+      .chain(filteredFontList)
       .sortBy(font => font.family)
       .take(numMaxVisible)
       .map(font => {
@@ -61,7 +116,7 @@ class FontsPage extends React.Component {
       .value()
 
     const btnSeeMore =
-      numMaxVisible > fontList.length ?
+      numMaxVisible > filteredFontList.length ?
         null:
         <div className="row see-more-wrapper">
           <div className="col-xs-12 text-center">
@@ -74,9 +129,53 @@ class FontsPage extends React.Component {
           </div>
         </div>
 
+    const toCheckboxLi = (categ, title) => {
+      const icon =
+        isChecked(fontCategories, categ) ?
+          <span className="custom-checkbox-icon fa fa-2x fa-check-square"></span> :
+          <span className="custom-checkbox-icon fa fa-2x fa-square"></span>
+
+      return (
+        <li className="li-fixed-width">
+          <div
+            className={`custom-checkbox ${isChecked(fontCategories, categ) ? "checked" : ""}`}
+            onClick={this.handleChangeCheckbox.bind(this, categ)}
+          >
+
+            {icon}
+            <span className="custom-checkbox-label">
+              {title}
+            </span>
+          </div>
+        </li>
+      )
+    }
+
     return (
       <div>
         <div className="container">
+
+          <div className="row margin-top-lg">
+            <div className="col-xs-12 col-lg-8">
+              <ul className="list-inline">
+                {toCheckboxLi("serif", "Serif")}
+                {toCheckboxLi("sans-serif", "Sans serif")}
+                {toCheckboxLi("display", "Cursive")}
+                {toCheckboxLi("handwriting", "Handwriting")}
+                {toCheckboxLi("monospace", "Monospace")}
+              </ul>
+            </div>
+            <div className="col-xs-12 col-lg-4">
+              <input
+                type="text"
+                className="form-control input-lg"
+                value={searchInput}
+                placeholder="Search"
+                onChange={this.handleChangeSearch.bind(this)}
+              />
+            </div>
+          </div>
+
           {fontsNode}
           {btnSeeMore}
         </div>
