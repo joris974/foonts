eac20d2cb17cdc767969d64c257edcd9c5f1858a
Joris Buchou <joris.buchou@gmail.com>
2020-02-23 19:46:40 -0800

    Add CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AppRouter } from "./routers/router";
 import * as serviceWorker from "./serviceWorker";
+import "./app.css";
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
