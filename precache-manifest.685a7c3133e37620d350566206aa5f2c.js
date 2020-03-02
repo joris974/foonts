dba8f75654d66e8f11b1ac45454076c38d7c02b7
Joris Buchou <joris.buchou@gmail.com>
2020-03-01 17:16:57 -0800

    Updates

---
+++ precache-manifest.685a7c3133e37620d350566206aa5f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9ecb85833b5d27b9b449d0ba750401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48a7b324496cc6ab342a",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ab11c347857d0341ea17",
+    "url": "/static/css/main.eb48fddf.chunk.css"
+  },
+  {
+    "revision": "48a7b324496cc6ab342a",
+    "url": "/static/js/2.71930bce.chunk.js"
+  },
+  {
+    "revision": "fa942519a7a9feed579ab048ae65ea42",
+    "url": "/static/js/2.71930bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab11c347857d0341ea17",
+    "url": "/static/js/main.ce3f493a.chunk.js"
+  },
+  {
+    "revision": "b7f061d7dd31127261e4",
+    "url": "/static/js/runtime-main.6d5213d1.js"
+  }
+]);
