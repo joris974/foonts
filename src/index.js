d8b219b604a4ca061528a6f9104358fae941b3c8
Joris Buchou <joris.buchou@gmail.com>
2020-03-22 18:01:07 -0700

    Remove import of bootstrap CSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AppRouter } from "./routers/router";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
