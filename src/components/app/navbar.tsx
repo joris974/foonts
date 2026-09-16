f46eb5fe495287144b9dee714bf7eba0f3ef9e3b
Joris Buchou <joris.buchou@gmail.com>
2026-09-15 22:12:09 -0700

    Format

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink  as RouterLink } from "react-router";
+import { NavLink as RouterLink } from "react-router";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 const Navbar = () => {
